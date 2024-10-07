f37ebec3dee8997fa770ae815ba43041afb22b40
toxic123MMM <aashish02@gmail.com>
2024-10-07 17:31:53 +0400

    Add files via upload

---
+++ And,or.py
@@ -0,0 +1,22 @@
+a = 12
+b = 10
+c = 0
+if a and b and c:
+    print("all the numbers have boolean value as true")
+else:
+    print("atleast one number has booolean value as false")
+
+
+a = 36
+b = -36
+c = 0
+if a>0 or b>0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+
+
+if b>0 or c>0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
